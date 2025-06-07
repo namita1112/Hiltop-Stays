dff0824bc946fcc4368d563d81b9d4a2a7cea83c
Namita Tare <namitatare116@gmail.com>
2025-06-07 17:15:08 +0530

    update app.jsx

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API working fine 🚀");
+});
+
 const hotelRoutes = require("./routes/hotelRoutes");
 app.use("/api", hotelRoutes);
 
